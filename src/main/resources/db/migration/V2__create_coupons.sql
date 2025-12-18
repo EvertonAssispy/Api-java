fb3623cd2fff9a5ed0ba6862c8cdc980b17c5d79
evertonkj <elima8183@gmail.com>
2025-12-18 15:51:55 -0300

    feat: add migrations

---
+++ src/main/resources/db/migration/V2__create_coupons.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE coupons(
+    id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+    discount INTEGER NOT NULL,
+    code VARCHAR(100) NOT NULL,
+    valid TIMESTAMP NOT NULL,
+    event_id UUID,
+    FOREIGN KEY (event_id) REFERENCES event(id) ON DELETE CASCADE
+);
