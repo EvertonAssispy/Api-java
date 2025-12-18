cac7c1ac1e5f478f5482dcc467a3411b4e2b4c17
evertonkj <elima8183@gmail.com>
2025-12-18 15:08:40 -0300

    feat: first commit

---
+++ src/main/java/com/everton/Teste_Api_Psql/TesteApiPsqlApplication.java
@@ -0,0 +1,13 @@
+package com.everton.Teste_Api_Psql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteApiPsqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteApiPsqlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everton/Teste_Api_Psql/TesteApiPsqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everton.Teste_Api_Psql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteApiPsqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
